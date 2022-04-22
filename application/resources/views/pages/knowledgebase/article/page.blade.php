8251f7b676a5c1c0967643773d9c84aa5075058f
Nuel <nuelsville@gmail.com>
2022-04-22 14:36:27 +0100

    DB

--- application/resources/views/pages/knowledgebase/article/page.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="card">
-    <div class="card-body">
-        <h4 class="card-title card-title-underlined">{{ $knowledgebase->knowledgebase_title }}</h4>
-        <p class="card-text">{!! _clean($knowledgebase->knowledgebase_text) !!}</p>
-    </div>
-</div>
